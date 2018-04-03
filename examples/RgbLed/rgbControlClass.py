31fa640957ca39668b0f5f5a7e1a0e2e0bfab642
Mümin Köykıran <muminkoykiran@gmail.com>
2018-04-04 00:36:04 +0300

    Update rgbControlClass.py

--- examples/RgbLed/rgbControlClass.py
+++ examples/RgbLed/rgbControlClass.py
@@ -5,7 +5,6 @@ if not os.getegid() == 0:
     sys.exit('Script must be run as root')
 
 from pyA20.gpio import gpio
-from pyA20.gpio import port
 
 class RGBControl:
     def __init__(self, red, green, blue):
